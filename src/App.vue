0242613cb00e798a6f8e553225fa356f30f6f788
mimoid-prog <mimoid.prog@gmail.com>
2020-05-06 14:41:56 +0200

    Added main layout and navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="main-layout">
+      <div class="container">
+        <div class="main-layout-inner">
+          <Navbar />
+          <div class="xd"></div>
+        </div>
+      </div>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
-#nav {
-  padding: 30px;
+body {
+  font-family: 'Jost', sans-serif;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.main-layout {
+  background: #ae0505;
+  width: 100%;
+  min-height: 100vh;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.container {
+  width: 85%;
+  margin: 0 auto;
+  padding: 30px 0;
+}
+
+.xd {
+  width: 100%;
+  height: 300px;
+  margin-top: 30px;
+  background: white;
+}
+
+@media (min-width: 850px) {
+  .main-layout-inner {
+    display: flex;
+  }
+
+  .xd {
+    margin-top: 0px;
+  }
+}
+
+@media (min-width: 1300px) {
+  .container {
+    width: 85%;
   }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,248 @@
+<template>
+  <div class="navbar">
+    <div class="titleBox">
+      <h1>
+        Tur
+        <span>Jaktorów</span>
+      </h1>
+    </div>
+    <div class="navBox">
+      <div class="hamburgerBox">
+        <button class="hamburgerToOpen" @click="active = true">
+          <div></div>
+        </button>
+      </div>
+      <div :class="[{ active }, 'navBoxInner']">
+        <button class="hamburgerToClose" @click="active = false">
+          <div></div>
+          <div></div>
+        </button>
+        <nav>
+          <ul class="menu">
+            <li>
+              <router-link to="/" class="test-active">Strona główna</router-link>
+            </li>
+            <li>
+              <router-link to="/aktualnosci">Aktualności</router-link>
+            </li>
+            <li>
+              <router-link to="/o-klubie">O klubie</router-link>
+            </li>
+            <li>
+              <router-link to="/galeria">Galeria zdjęć</router-link>
+            </li>
+            <li>
+              <router-link to="/terminarz">Terminarz</router-link>
+            </li>
+            <li>
+              <router-link to="/informacje">Informacje</router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data: function() {
+    return {
+      active: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+  color: white;
+  display: flex;
+  justify-content: space-between;
+}
+
+h1 {
+  font-size: 70px;
+  line-height: 1;
+  text-transform: uppercase;
+
+  span {
+    font-size: 40px;
+    display: block;
+    transform: translateX(10px) translateY(-8px);
+  }
+}
+
+.hamburgerToOpen {
+  border: none;
+  display: block;
+  height: 60px;
+  background: none;
+  box-sizing: content-box;
+  padding: 0px 10px;
+
+  div {
+    width: 42px;
+    height: 6px;
+    background: white;
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      width: 42px;
+      height: 6px;
+      background: white;
+    }
+
+    &:before {
+      transform: translateY(-12px);
+    }
+
+    &:after {
+      transform: translateY(6px);
+    }
+  }
+}
+
+.navBoxInner {
+  position: fixed;
+  background: #1a1a1a;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  padding: 30px;
+  transition: transform 0.5s ease-in-out;
+  transform: translateX(100%);
+}
+
+.navBoxInner.active {
+  transform: translateX(0);
+}
+
+.hamburgerToClose {
+  border: none;
+  display: block;
+  background: none;
+  width: 42px;
+  height: 60px;
+  position: relative;
+  padding: 0 10px;
+  margin: 0 auto;
+  box-sizing: content-box;
+
+  div {
+    width: 42px;
+    height: 6px;
+    background: white;
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+
+    &:first-child {
+      transform: rotate(45deg);
+    }
+
+    &:last-child {
+      transform: rotate(-45deg);
+    }
+  }
+}
+
+nav {
+  height: 100%;
+  margin: 20px 0;
+}
+
+.menu {
+  list-style: none;
+  text-align: center;
+  height: 100%;
+}
+
+li {
+  margin-bottom: 25px;
+
+  &:last-child {
+    margin: 0;
+  }
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  font-size: 22px;
+  font-weight: 700;
+}
+
+.test-active {
+  border-bottom: 4px solid white;
+}
+
+@media (min-width: 850px) {
+  .hamburgerToOpen,
+  .hamburgerToClose,
+  .hamburgerBox {
+    display: none;
+  }
+
+  nav {
+    margin: 0;
+  }
+
+  .navBox {
+    margin-top: 30px;
+  }
+
+  .navBoxInner {
+    transform: translateX(0);
+    background: none;
+    position: static;
+    padding: 0;
+  }
+
+  .menu {
+    text-align: left;
+  }
+
+  .navbar {
+    flex-direction: column;
+    padding-right: 50px;
+  }
+}
+
+@media (min-width: 1024px) {
+  li {
+    margin-bottom: 20px;
+    transition: transform 0.2s ease;
+
+    &:hover {
+      transform: translateX(2px);
+    }
+  }
+}
+
+@media (min-width: 1300px) {
+  h1 {
+    font-size: 100px;
+
+    span {
+      font-size: 50px;
+      transform: translateX(18px) translateY(-8px);
+    }
+  }
+
+  .navbar {
+    padding-right: 100px;
+  }
+
+  .navBox {
+    padding-left: 10px;
+  }
+
+  a {
+    font-size: 28px;
+  }
+}
+</style>
