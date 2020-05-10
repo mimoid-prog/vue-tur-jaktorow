b18832c9ae903dbeb7c284af43b5f06c221b88eb
mimoid-prog <mimoid.prog@gmail.com>
2020-05-10 14:24:02 +0200

    Added information page

--- src/views/Information.vue
+++ src/views/Information.vue
@@ -2,7 +2,15 @@
   <div class="information">
     <h2 class="secondaryTitle viewTitle">Informacje</h2>
     <div class="content">
-      <h3>Masz jakieś uwagi?</h3>
+      <p>
+        Strona Tur Jaktorów pod adresem
+        <b>tur-jaktorow.now.sh</b> to nieoficjalna, fanowska strona Jaktorowskiego klubu piłkarskiego.
+      </p>
+      <p>Masz jakieś uwagi/spostrzeżenia dotyczące strony lub pomysł co można ulepszyć/poprawić?</p>
+      <p>Chcesz się o coś zapytać lub skontaktować w ramach współpracy?</p>
+      <p class="contact-me">Napisz do mnie:</p>
+      <p class="email">mateusz.szkop99@gmail.com</p>
+      <p class="copyright">© 2020 Wszelkie prawa zastrzeżone</p>
     </div>
   </div>
 </template>
@@ -21,5 +29,34 @@ export default {
 .content {
   background: white;
   padding: 20px;
+}
+
+p {
+  margin-top: 3px;
+
+  &:first-child {
+    margin-bottom: 14px;
+  }
+}
+
+.contact-me {
+  font-weight: 700;
+}
+
+.email {
+  background: #f1f1f1;
+  padding: 5px 10px;
+  border-radius: 5px;
+}
+
+.copyright {
+  margin-top: 30px;
+  font-weight: 700;
+}
+
+@media (min-width: 1024px) {
+  .content {
+    padding: 30px;
+  }
 }
 </style>
