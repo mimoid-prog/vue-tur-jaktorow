43a80f67ba6892a3f0e56b97c59c436b755cf84b
mimoid-prog <mimoid.prog@gmail.com>
2020-05-10 14:38:22 +0200

    Added Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueAnalytics from "vue-analytics";
 //import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+Vue.use(VueAnalytics, {
+  id: "UA-156522420-4",
+});
 
 new Vue({
   router,
